d72de1fa12956f4199c07a67fa009c606e3e245a
AlexeyZabalotcki <AlexeyZabalotckiBusiness@gmail.com>
2022-10-04 12:56:04 +0300

    Created custom task table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CategoriesComponent } from './views/categories/categories.component';
-import { TasksComponent } from './views/tasks/tasks.component';
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from './views/categories/categories.component';
+import {TasksComponent} from './views/tasks/tasks.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -12,9 +16,14 @@ import { TasksComponent } from './views/tasks/tasks.component';
     TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
